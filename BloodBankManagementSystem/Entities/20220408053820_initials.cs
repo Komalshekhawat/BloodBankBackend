ad540407ace794e94b31cfdcb6db64dd165a21d8
Komalshekhawat <75672726+Komalshekhawat@users.noreply.github.com>
2022-04-09 21:31:02 +0530

    backend

---
+++ BloodBankManagementSystem/Entities/20220408053820_initials.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodBankManagementSystem.Migrations
+{
+    public partial class initials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodDonorDonations_Blooddonors_BloodDonorID",
+                table: "BloodDonorDonations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BloodDonorDonations_BloodDonorID",
+                table: "BloodDonorDonations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodDonorDonations_BloodDonorID",
+                table: "BloodDonorDonations",
+                column: "BloodDonorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodDonorDonations_Blooddonors_BloodDonorID",
+                table: "BloodDonorDonations",
+                column: "BloodDonorID",
+                principalTable: "Blooddonors",
+                principalColumn: "BloodDonorID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
